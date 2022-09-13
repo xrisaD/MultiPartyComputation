65354761779b911baa1243ec354bd2611b70c61d
xrisaD <xrdikonimaki@hotmail.com>
2022-09-13 23:01:56 +0200

    run file: general stucture

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 from ttp.server import runTTP
+from plot.plot import plot
 
-res = runTTP(times=range(5, 0, -1))
+
+num_of_users = [30, 40, 50]
+time_to_compute_mean = []
+
+for u in num_of_users:
+    t = runTTP(times=range(u, 0, -1))
+    time_to_compute_mean.append(t)
+
+plot(num_of_users, time_to_compute_mean)
