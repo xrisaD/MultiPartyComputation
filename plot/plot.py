81a3472c5eae423de97e3d0eeb79268864c6174d
xrisaD <xrdikonimaki@hotmail.com>
2022-09-13 23:07:32 +0200

    plot for both

--- plot/plot.py
+++ plot/plot.py
@@ -5,4 +5,5 @@ import numpy as np
 def plot(x, y_ttp, y_mpc):
     plt.plot(np.array(x), np.array(y_ttp), label="TTP")
     plt.plot(np.array(x), np.array(y_mpc), label="MPC")
+    plt.legend()
     plt.show()
